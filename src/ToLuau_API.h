387a8622b0ca24134d3b56d6dc0bb64aaf429297
ResetOTER <vgvgvvvdc@gmail.com>
2022-06-19 18:39:03 +0800

    init luau

---
+++ src/ToLuau_API.h
@@ -0,0 +1,42 @@
+
+#ifndef ToLuau_API_H
+#define ToLuau_API_H
+
+#ifdef ToLuau_BUILT_AS_STATIC
+#  define ToLuau_API
+#  define TOLUAU_NO_EXPORT
+#else
+#  ifndef ToLuau_API
+#    ifdef ToLuau_EXPORTS
+        /* We are building this library */
+#      define ToLuau_API __declspec(dllexport)
+#    else
+        /* We are using this library */
+#      define ToLuau_API __declspec(dllimport)
+#    endif
+#  endif
+
+#  ifndef TOLUAU_NO_EXPORT
+#    define TOLUAU_NO_EXPORT 
+#  endif
+#endif
+
+#ifndef TOLUAU_DEPRECATED
+#  define TOLUAU_DEPRECATED __declspec(deprecated)
+#endif
+
+#ifndef TOLUAU_DEPRECATED_EXPORT
+#  define TOLUAU_DEPRECATED_EXPORT ToLuau_API TOLUAU_DEPRECATED
+#endif
+
+#ifndef TOLUAU_DEPRECATED_NO_EXPORT
+#  define TOLUAU_DEPRECATED_NO_EXPORT TOLUAU_NO_EXPORT TOLUAU_DEPRECATED
+#endif
+
+#if 0 /* DEFINE_NO_DEPRECATED */
+#  ifndef TOLUAU_NO_DEPRECATED
+#    define TOLUAU_NO_DEPRECATED
+#  endif
+#endif
+
+#endif /* ToLuau_API_H */
---
+++ test/TestToLuau.cpp
@@ -0,0 +1,8 @@
+//
+// Created by 35207 on 2022/6/19 0019.
+//
+
+int main()
+{
+	return 0;
+}
