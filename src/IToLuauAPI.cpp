af957f45c5da24caa5a3f220858018ed5ddb90fa
ResetOTER <vgvgvvvdc@gmail.com>
2022-06-21 20:22:28 +0800

    更新

--- src/IToLuauAPI.cpp
+++ src/IToLuauAPI.cpp
@@ -6,5 +6,35 @@
 
 namespace ToLuau
 {
+	void IToLuauAPI::BeginModule(const std::string& ModuleName)
+	{
+	}
 
+	void IToLuauAPI::EndModule()
+	{
+	}
+
+	void IToLuauAPI::BeginClass(const std::string& ClassName, const std::string& SuperClassName)
+	{
+	}
+
+	void IToLuauAPI::EndClass()
+	{
+	}
+
+	int IToLuauAPI::BeginEnum(const std::string& EnumName)
+	{
+	}
+
+	void IToLuauAPI::EndEnum()
+	{
+	}
+
+	void IToLuauAPI::RegFunction(const std::string& FuncName, LuaFunc Func)
+	{
+	}
+
+	void IToLuauAPI::RegVar(const std::string& VarName, LuaFunc Setter, LuaFunc Getter)
+	{
+	}
 }
--- src/IToLuauAPI.h
+++ src/IToLuauAPI.h
@@ -1,11 +1,29 @@
 #pragma once
 
 #include <string>
+#include <functional>
+
+#include "lua.h"
 
 namespace ToLuau
 {
 	class IToLuauAPI
 	{
-		
+	public:
+
+		using LuaFunc = std::function<int(lua_State*)>;
+
+		void BeginModule(const std::string& ModuleName);
+		void EndModule();
+
+		void BeginClass(const std::string& ClassName, const std::string& SuperClassName);
+		void EndClass();
+
+		int BeginEnum(const std::string& EnumName);
+		void EndEnum();
+
+		void RegFunction(const std::string& FuncName, LuaFunc Func);
+
+		void RegVar(const std::string& VarName, LuaFunc Setter, LuaFunc Getter);
 	};
 }
