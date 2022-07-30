7203c03620dd3c6407514f7665580e4a422cffda
ResetOTER <vgvgvvvdc@gmail.com>
2022-07-30 12:23:30 +0800

    update

--- src/Toluau/API/MetaData.cpp
+++ src/Toluau/API/MetaData.cpp
@@ -2,4 +2,107 @@
 // Created by chen.dong on 7/25/2022.
 //
 #include "MetaData.h"
+#include <cassert>
+#include "Toluau/Util/Util.h"
 
+namespace ToLuau
+{
+
+	NamespaceMetaData& ToLuau::LuaMetaData::PushNamespace(const std::string &Name)
+	{
+		auto& Scope = ScopeStack.back();
+		if(Scope->Type() == ScopeType::Namespace)
+		{
+			auto NamespaceScope = std::static_pointer_cast<NamespaceMetaData>(Scope);
+			auto It = NamespaceScope->Namespaces.find(Name);
+			if(It != NamespaceScope->Namespaces.end())
+			{
+				ScopeStack.push_back(It->second);
+				return *It->second;
+			}
+			else
+			{
+				auto NewNamespace = std::make_shared<NamespaceMetaData>(Name);
+				NamespaceScope->Namespaces.insert({Name, NewNamespace});
+				ScopeStack.push_back(NewNamespace);
+				return *NewNamespace;
+			}
+		}
+		else
+		{
+			LUAU_ERROR_F("Cannot push %s name space in current scope", Name.c_str());
+			assert(false);
+		}
+	}
+
+	ClassMetaData& LuaMetaData::PushClass(const std::string &Name, const std::string &SuperName)
+	{
+		auto& Scope = ScopeStack.back();
+		if (Scope->Type() == ScopeType::Namespace)
+		{
+			auto NamespaceScope = std::static_pointer_cast<NamespaceMetaData>(Scope);
+			auto It = NamespaceScope->Classes.find(Name);
+			if(It != NamespaceScope->Classes.end())
+			{
+				ScopeStack.push_back(It->second);
+				return *It->second;
+			}
+			else
+			{
+				auto NewClass = std::make_shared<ClassMetaData>(Name, SuperName);
+				NamespaceScope->Classes.insert({Name, NewClass});
+				ScopeStack.push_back(NewClass);
+				return *NewClass;
+			}
+		}
+		else
+		{
+			LUAU_ERROR_F("Cannot push %s name space in current scope", Name.c_str());
+			assert(false);
+		}
+	}
+
+	EnumMetaData& LuaMetaData::PushEnum(const std::string &Name)
+	{
+		auto& Scope = ScopeStack.back();
+		if (Scope->Type() == ScopeType::Namespace)
+		{
+			auto NamespaceScope = std::static_pointer_cast<NamespaceMetaData>(Scope);
+			auto It = NamespaceScope->Enums.find(Name);
+			if(It != NamespaceScope->Enums.end())
+			{
+				ScopeStack.push_back(It->second);
+				return *It->second;
+			}
+			else
+			{
+				auto NewEnum = std::make_shared<EnumMetaData>(Name);
+				NamespaceScope->Enums.insert({Name, NewEnum});
+				ScopeStack.push_back(NewEnum);
+				return *NewEnum;
+			}
+		}
+		else
+		{
+			LUAU_ERROR_F("Cannot push %s name space in current scope", Name.c_str());
+			assert(false);
+		}
+	}
+
+	void LuaMetaData::RegVar(bool IsStatic, const std::string &TypeName, const std::string &VarName)
+	{
+		auto& Scope = ScopeStack.back();
+
+		std::shared_ptr<FieldMetaData>
+	}
+
+	void LuaMetaData::Pop()
+	{
+		assert(!ScopeStack.empty());
+		ScopeStack.pop_back();
+	}
+
+
+
+
+}
--- src/Toluau/API/MetaData.h
+++ src/Toluau/API/MetaData.h
@@ -4,19 +4,17 @@
 #include <vector>
 #include <map>
 #include <memory>
+#include <cassert>
+
+#include "Toluau/Util/Util.h"
 
 namespace ToLuau
 {
     enum class ScopeType
     {
         Namespace,
-        Class
-    };
-
-    class IMetaDataScope
-    {
-    public:
-        virtual ScopeType Type() const = 0;
+        Class,
+		Enum
     };
 
     class  FunctionMetaData
@@ -39,38 +37,83 @@ namespace ToLuau
         std::string Type;
     };
 
+	class IMetaDataScope
+	{
+	public:
+		virtual ScopeType Type() const = 0;
+		virtual void RegVar(const std::shared_ptr<FieldMetaData>& InField) { LUAU_ERROR("Cannot reg var in current scope !!") }
+		virtual void RegFunction(const std::shared_ptr<FunctionGroupMetaData>& InField) { LUAU_ERROR("Cannot reg var in current scope !!") }
+	};
+
+	class EnumMetaData : public IMetaDataScope
+	{
+		friend class LuaMetaData;
+	public:
+		explicit EnumMetaData(const std::string& InName)
+		{
+			Name = InName;
+		}
+		virtual ScopeType Type() const override { return ScopeType::Enum; }
+
+	private:
+		std::string Name;
+		std::map<std::string, std::shared_ptr<FieldMetaData>> Fields;
+	};
+
     class  ClassMetaData : public IMetaDataScope
     {
+	    friend class LuaMetaData;
     public:
+	    ClassMetaData(const std::string& InName, const std::string& InSuperClass)
+	    {
+			Name = InName;
+		}
         virtual ScopeType Type() const override { return ScopeType::Class; }
 
     private:
         std::string Name;
+		std::string SuperClass;
         std::map<std::string, std::shared_ptr<FunctionGroupMetaData>> Functions;
         std::map<std::string, std::shared_ptr<FieldMetaData>> Fields;
     };
 
     class  NamespaceMetaData : public IMetaDataScope
     {
+		friend class LuaMetaData;
     public:
+	    explicit NamespaceMetaData(const std::string& InName)
+	    {
+			Name = InName;
+		}
         virtual ScopeType Type() const override { return ScopeType::Namespace; }
 
     private:
         std::string Name;
         std::map<std::string, std::shared_ptr<ClassMetaData>> Classes;
+	    std::map<std::string, std::shared_ptr<EnumMetaData>> Enums;
+	    std::map<std::string, std::shared_ptr<NamespaceMetaData>> Namespaces;
+	    std::map<std::string, std::shared_ptr<FunctionGroupMetaData>> Functions;
+	    std::map<std::string, std::shared_ptr<FieldMetaData>> Fields;
     };
 
     class LuaMetaData
     {
     public:
+	    LuaMetaData()
+	    {
+			GlobalMetaData = std::make_shared<NamespaceMetaData>("");
+		    ScopeStack.push_back(GlobalMetaData);
+		}
+
         NamespaceMetaData& PushNamespace(const std::string& Name);
         ClassMetaData& PushClass(const std::string& Name, const std::string& SuperName);
+	    EnumMetaData& PushEnum(const std::string& Name);
 
-        void RegVar(const std::string& TypeName, const std::string& VarName);
+        void RegVar(bool IsStatic, const std::string& TypeName, const std::string& VarName);
 
         void Pop();
     private:
-        std::map<std::string, std::shared_ptr<NamespaceMetaData>> ClassMetaData;
+	    std::shared_ptr<NamespaceMetaData> GlobalMetaData;
         std::vector<std::shared_ptr<IMetaDataScope>> ScopeStack;
     };
 }
