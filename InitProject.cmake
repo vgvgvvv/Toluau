3fc6803ff384b193c1a47b5adf2ea1595e9014f6
ResetOTER <vgvgvvvdc@gmail.com>
2023-01-07 12:23:49 +0800

    use standalone luau

--- InitProject.cmake
+++ InitProject.cmake
@@ -11,15 +11,6 @@ macro(InitProject)
     )
     FetchContent_MakeAvailable(re-cmake)
 
-    message(STATUS "fetch re-third-part library")
-    include(FetchContent)
-    FetchContent_Declare(
-            re-third-part
-            GIT_REPOSITORY https://github.com/vgvgvvv/re-third-part
-            GIT_TAG        "v1.0"
-    )
-    FetchContent_MakeAvailable(re-third-part)
-
     message(STATUS "re-cmake_SOURCE_DIR ${re-cmake_SOURCE_DIR}")
     list(APPEND CMAKE_MODULE_PATH "${re-cmake_SOURCE_DIR}")
     include(Init)
